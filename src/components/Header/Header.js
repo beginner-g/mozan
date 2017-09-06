1d1d13915659e5d94cd99310d338cc5ee7746639
beginner-g <969925523@qq.com>
2017-09-06 14:52:11 +0800

    mozan

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Header extends Components{
+  render(){
+    return(
+      <div className="header">
+        
+      </div>
+    )
+  }
+}
+export default Header
