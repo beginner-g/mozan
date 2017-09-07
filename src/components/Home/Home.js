e5571c057df3208773be5871c436089c2bcc0efe
beginner-g <969925523@qq.com>
2017-09-07 22:41:15 +0800

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,18 +4,25 @@ import Banner from '../Home/Banner/Banner'
 import Count_down from './Count_down/Count_down'
 import Choose from './Choose/Choose'
 import './home.css';
+import img from '../../images/pic_03.png'
 class Home extends Component{
   render() {
     return (
       <div className="home1">
         <div className="main">
-        <Banner/>
-        <div className="main_m">
-          <div className="main_con">
-          <Count_down/>
-          <Choose/>
+          <Banner/>
+          <div className="main_m">
+            <div className="main_con">
+              <Count_down/>
+              <Choose/>
+            </div>
+            <div className="button">
+                <a href='#'>立即预购</a>
+            </div>
           </div>
-        </div>
+          <div className="images">
+            <img src={img} alt="" />
+          </div>
         </div>
       </div>
     );
