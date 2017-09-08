8d3d0227d2ba0865eacad3491d343f50ec540815
beginner-g <969925523@qq.com>
2017-09-08 15:07:55 +0800

    join_cart

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Layout from '../Layout/Layout'
 import Home from '../Home/Home'
+import Join_cart from '../Join_cart/Join_cart'
 import './app.css';
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Layout>
-          <Home/>
+          {/* <Home/> */}
+        <Join_cart/>
         </Layout>
       </div>
     );
